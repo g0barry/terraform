dbd0659767846c1d372f53c3d17ee0db6d2a36b0
g0barry <g0barry@gmail.com>
2020-04-29 09:59:59 -0700

    now with output

--- main.tf
+++ main.tf
@@ -4,11 +4,12 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami           = "ami-09dd2e08d601bff67"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
   
   user_data = <<-EOF
               #!/bin/bash
               echo "hello, world" > index.html
-              nohup busybox httpd -f -p 8080 &
+              nohup busybox httpd -f -p ${var.server_port} &
               EOF
  
   tags = { 
@@ -19,9 +20,18 @@ resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
 
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = var.server_port
+    to_port     = var.server_port
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type          = number
+  default       = 8080
+}
+output "public_ip" {
+  value       = aws_instance.example.public_ip
+  description = "the public IP address of the web server"
 }
